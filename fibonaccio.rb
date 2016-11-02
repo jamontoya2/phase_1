36a3504c6c4a3ba247a0804dc9a02e58122f6b1e
codeamac01 <codeamac01@gmail.com>
2016-11-02 10:33:04 -0600

    fibo recursive and iterative with benchmark

---
+++ fibonaccio.rb
@@ -0,0 +1,45 @@
+def fibonacci_iterative(num)
+  primero=0
+  siguiente=1
+    (1..num).each do 
+      temporal = primero
+      primero = siguiente 
+      siguiente = temporal + siguiente
+    end
+  primero  
+end
+
+module Fib
+  @@mem = {}
+  def self.compute(index)
+    if index <= 1
+      return index
+    else
+      @@mem[index] ||= compute(index-1) +  compute(index-2)
+    end
+  end
+end
+
+p fibonacci_iterative(0) == 0
+p fibonacci_iterative(1) == 1
+p fibonacci_iterative(2) == 1
+p fibonacci_iterative(3) == 2
+p fibonacci_iterative(4) == 3
+p fibonacci_iterative(5) == 5
+
+p Fib.compute(0) == 0
+p Fib.compute(1) == 1
+p Fib.compute(2) == 1
+p Fib.compute(3) == 2
+p Fib.compute(4) == 3
+p Fib.compute(5) == 5
+
+
+
+require 'benchmark'
+num = 1000
+Benchmark.bm do |x|
+  x.report("iterative_fib") { fibonacci_iterative(num) }
+  x.report("recursive_fib") { Fib.compute(num)  }
+end
+
